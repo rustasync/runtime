95414d626805c0525580598e09bcf961d9bcc110
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-05-08 12:35:42 +0200

    add wasm32-unknown-unknown support to runtime-native (#22)
    * update versions
    
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>
    
    * runtime 0.3.0-alpha.3
    
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>
    
    * add wasm32-unknown-unknown support
    
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>
    
    * reorder cfgs
    
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>
    
    * Update runtime-native/src/wasm32.rs
    
    Co-Authored-By: yoshuawuyts <yoshuawuyts+github@gmail.com>
    
    * wasm-bindgen feature flag
    
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>
    
    * remove unneeded combinators
    
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- runtime-native/src/lib.rs
+++ runtime-native/src/lib.rs
@@ -10,64 +10,12 @@
     rust_2018_idioms
 )]
 
-use futures::prelude::*;
-use futures::{future::BoxFuture, task::SpawnError};
-use lazy_static::lazy_static;
+#[cfg(all(feature = "wasm-bindgen", target_arch = "wasm32"))]
+mod wasm32;
+#[cfg(all(feature = "wasm-bindgen", target_arch = "wasm32"))]
+pub use wasm32::Native;
 
-use std::io;
-use std::net::SocketAddr;
-use std::pin::Pin;
-
-mod tcp;
-mod udp;
-
-use tcp::{TcpListener, TcpStream};
-use udp::UdpSocket;
-
-lazy_static! {
-    static ref JULIEX_THREADPOOL: juliex::ThreadPool = {
-        juliex::ThreadPool::with_setup(|| {
-            runtime_raw::set_runtime(&Native);
-        })
-    };
-}
-
-/// The Native runtime.
-#[derive(Debug)]
-pub struct Native;
-
-impl runtime_raw::Runtime for Native {
-    fn spawn_boxed(&self, fut: BoxFuture<'static, ()>) -> Result<(), SpawnError> {
-        JULIEX_THREADPOOL.spawn_boxed(fut.into());
-        Ok(())
-    }
-
-    fn connect_tcp_stream(
-        &self,
-        addr: &SocketAddr,
-    ) -> BoxFuture<'static, io::Result<Pin<Box<dyn runtime_raw::TcpStream>>>> {
-        let romio_connect = romio::TcpStream::connect(addr);
-        let connect = romio_connect.map(|res| {
-            res.map(|romio_stream| {
-                Box::pin(TcpStream { romio_stream }) as Pin<Box<dyn runtime_raw::TcpStream>>
-            })
-        });
-        connect.boxed()
-    }
-
-    fn bind_tcp_listener(
-        &self,
-        addr: &SocketAddr,
-    ) -> io::Result<Pin<Box<dyn runtime_raw::TcpListener>>> {
-        let romio_listener = romio::TcpListener::bind(&addr)?;
-        Ok(Box::pin(TcpListener { romio_listener }))
-    }
-
-    fn bind_udp_socket(
-        &self,
-        addr: &SocketAddr,
-    ) -> io::Result<Pin<Box<dyn runtime_raw::UdpSocket>>> {
-        let romio_socket = romio::UdpSocket::bind(&addr)?;
-        Ok(Box::pin(UdpSocket { romio_socket }))
-    }
-}
+#[cfg(not(all(feature = "wasm-bindgen", target_arch = "wasm32")))]
+mod not_wasm32;
+#[cfg(not(all(feature = "wasm-bindgen", target_arch = "wasm32")))]
+pub use not_wasm32::Native;
---
+++ runtime-native/src/wasm32.rs
@@ -0,0 +1,44 @@
+use futures::prelude::*;
+use futures::{future::BoxFuture, task::SpawnError};
+// use futures::compat::*;
+
+use std::io;
+use std::net::SocketAddr;
+use std::pin::Pin;
+
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::future_to_promise;
+
+/// The Native runtime.
+#[derive(Debug)]
+pub struct Native;
+
+impl runtime_raw::Runtime for Native {
+    fn spawn_boxed(&self, fut: BoxFuture<'static, ()>) -> Result<(), SpawnError> {
+        use futures01::future::Future;
+        let fut = fut.unit_error().compat();
+        wasm_bindgen_futures::spawn_local(fut);
+        Ok(())
+    }
+
+    fn connect_tcp_stream(
+        &self,
+        _addr: &SocketAddr,
+    ) -> BoxFuture<'static, io::Result<Pin<Box<dyn runtime_raw::TcpStream>>>> {
+        panic!("Connecting TCP streams is currently not supported in wasm");
+    }
+
+    fn bind_tcp_listener(
+        &self,
+        _addr: &SocketAddr,
+    ) -> io::Result<Pin<Box<dyn runtime_raw::TcpListener>>> {
+        panic!("Binding TCP listeners is currently not supported in wasm");
+    }
+
+    fn bind_udp_socket(
+        &self,
+        _addr: &SocketAddr,
+    ) -> io::Result<Pin<Box<dyn runtime_raw::UdpSocket>>> {
+        panic!("Binding UDP sockets is currently not supported in wasm");
+    }
+}
