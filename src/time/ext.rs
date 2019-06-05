8ae5b06fe612fa1aa78f441b3b6126965d30a709
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-06-05 13:39:13 +0200

    impl ext
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- src/time/ext.rs
+++ src/time/ext.rs
@@ -1,54 +1,74 @@
 //! Extensions for Futures types.
 
 use std::error::Error;
-use std::fmt::{self, Debug, Display};
+use std::fmt;
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::time::{Duration, Instant};
 
+use super::Delay;
+
 /// The Future returned from [`FutureExt`].
 ///
 /// [`FutureExt.timeout`]: trait.FutureExt.html
 #[derive(Debug)]
-pub struct Timeout<T> {
-    _phantom: std::marker::PhantomData<T>,
+pub struct Timeout<F: Future> {
+    future: F,
+    delay: Delay,
 }
 
-impl<T: Future> Future for Timeout<T> {
-    type Output = Result<<T as Future>::Output, TimeoutError>;
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
-        unimplemented!();
+impl<F: Future> Timeout<F> {
+    pin_utils::unsafe_pinned!(future: F);
+    pin_utils::unsafe_pinned!(delay: Delay);
+}
+
+impl<F: Future> Future for Timeout<F> {
+    type Output = Result<F::Output, TimeoutError>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.as_mut().future().poll(cx) {
+            Poll::Pending => {}
+            Poll::Ready(t) => return Poll::Ready(Ok(t)),
+        }
+
+        if self.as_mut().poll(cx).is_ready() {
+            let err = Err(TimeoutError(Instant::now()));
+            Poll::Ready(err)
+        } else {
+            Poll::Pending
+        }
     }
 }
 
 /// The Error returned from [`Timeout`].
 ///
 /// [`Timeout`]: struct.Timeout.html
+#[derive(Debug)]
 pub struct TimeoutError(pub Instant);
 impl Error for TimeoutError {}
 
-impl Debug for TimeoutError {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        unimplemented!();
-    }
-}
-
-impl Display for TimeoutError {
-    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        unimplemented!();
+impl fmt::Display for TimeoutError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        fmt::Debug::fmt(self, f)
     }
 }
 
 /// Extend `Future` with methods to time out execution.
 pub trait FutureExt: Future + Sized {
-    /// Timeout the future if it isn't completed after `dur` duration.
-    fn timeout(self, _dur: Duration) -> Timeout<Self> {
-        unimplemented!();
+    /// Time out the future if it isn't completed after `dur` duration.
+    fn timeout(self, dur: Duration) -> Timeout<Self> {
+        Timeout {
+            delay: Delay::new(dur),
+            future: self,
+        }
     }
 
-    /// Timeout the future if it isn't completed by `at`.
-    fn deadline(self, _at: Instant) -> Timeout<Self> {
-        unimplemented!();
+    /// Time out the future if it isn't completed before `at`.
+    fn timeout_at(self, at: Instant) -> Timeout<Self> {
+        Timeout {
+            delay: Delay::new_at(at),
+            future: self,
+        }
     }
 }
